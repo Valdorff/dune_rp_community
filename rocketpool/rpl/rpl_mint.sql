bcafcd233819f3eca81cce231e83334bbebc02bd
mtitus6 <84806551+mtitus6@users.noreply.github.com>
2024-02-12 06:04:21 -0500

    readme, folder structure, lst_supply brainstorming
    Added a starting readme file.  Added a folder structure
    Added column structure for _mint, _burn, _supply queries for review.

---
+++ rocketpool/rpl/rpl_mint.sql
@@ -0,0 +1,10 @@
+/*Include Dune query number*/
+
+/* block_time */
+/* tx_hash
+/* token_mint_amount_raw */
+/* token_mint_amount */
+/* token_contract_address */
+/* token_name */
+/* token_decimals */
+
