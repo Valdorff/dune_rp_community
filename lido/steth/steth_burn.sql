0c5e85189bd5b4729ba2ae5f7b0c456d1d552295
mtitus6 <84806551+mtitus6@users.noreply.github.com>
2024-02-24 19:11:17 -0500

    Add steth mint, burn, and supply

--- lido/steth/steth_burn.sql
+++ lido/steth/steth_burn.sql
@@ -1,10 +1,12 @@
-/*Include Dune query number*/
-
-/* block_time */
-/* tx_hash
-/* token_burn_amount_raw */
-/* token__amoburnunt */
-/* token_contract_address */
-/* token_name */
-/* token_decimals */
+/* Dune query number  - 3463911 */
+SELECT 
+    date_trunc('day', block_time) as day,
+    0xae7ab96520de3a18e5e111b5eaab095312d7fe84 as token_contract_address,
+    'stETH' as token_name,
+    SUM(CASE WHEN amount BETWEEN 20000000000 AND 32000000000 THEN CAST(amount AS DOUBLE)/1e9 
+    WHEN amount > 32000000000 THEN 32 ELSE 0 END) AS token_burn_amount
+FROM ethereum.withdrawals
+WHERE address = 0xB9D7934878B5FB9610B3fE8A5e441e8fad7E293f --withdrawl vault
+    AND amount >= 20000000000 
+GROUP BY 1
 
--- lido/steth/steth_mint.sql
+++ lido/steth/steth_mint.sql
@@ -1,10 +1,21 @@
-/*Include Dune query number*/
-
-/* block_time */
-/* tx_hash
-/* token_mint_amount_raw */
-/* token_mint_amount */
-/* token_contract_address */
-/* token_name */
-/* token_decimals */
-
+/* Dune query number  - 3463887 */
+SELECT
+  date_trunc('day', block_time) as day,
+  0xae7ab96520de3a18e5e111b5eaab095312d7fe84 as token_contract_address,
+  'stETH' as token_name,
+  SUM(cast(value AS DOUBLE)) / 1e18 AS token_mint_amount
+FROM
+  evms.traces
+WHERE
+  to = 0x00000000219ab540356cbb839cbe05303d7705fa -- beacon contract
+  AND blockchain = 'ethereum'
+  AND block_time >= CAST('2020-10-01' as timestamp)
+  AND call_type = 'call'
+  AND success = True
+  AND "from" in (
+    0xae7ab96520de3a18e5e111b5eaab095312d7fe84 --stETH contract
+    ,0xB9D7934878B5FB9610B3fE8A5e441e8fad7E293f --withdrawl vault
+    ,0xFdDf38947aFB03C621C71b06C9C70bce73f12999 --staking router
+  ) 
+GROUP BY
+  1
