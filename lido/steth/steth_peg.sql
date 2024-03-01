64c7422aadeafbb02fc00c2d3c986d9018230ed3
mtitus6 <84806551+mtitus6@users.noreply.github.com>
2024-02-29 21:41:41 -0500

    add steth peg, trades, price_peg_ratio

---
+++ lido/steth/steth_peg.sql
@@ -0,0 +1,8 @@
+/* Dune query number  - 3480196 */
+SELECT 
+    11473216 as block
+    , cast('2020-12-17 09:59:56' as TIMESTAMP) as time
+    , cast('2020-12-17' as TIMESTAMP) as day
+    , 0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84 as token_contract_address
+    , 'stETH' as token_name
+    , 1 as  token_peg_eth
