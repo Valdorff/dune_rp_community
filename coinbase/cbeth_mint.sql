cce278096dd54c105d3f20d37ca5f5bb123615d1
mtitus6 <84806551+mtitus6@users.noreply.github.com>
2024-03-11 22:24:41 -0400

    add sqlfluff config and reformat coinbase

--- coinbase/cbeth_mint.sql
+++ coinbase/cbeth_mint.sql
@@ -1,14 +1,14 @@
 /* Dune query number  - 3440999 */
-SELECT
-  date_trunc('day', evt_block_time) as day,
-  contract_address as token_contract_address,
-  'cbETH' as token_name,
-  SUM(value / CAST(1e18 AS DOUBLE)) AS token_mint_amount
-FROM
-  evms.erc20_transfers
-WHERE
-  contract_address = 0xBe9895146f7AF43049ca1c1AE358B0541Ea49704
-  AND "from" = 0x0000000000000000000000000000000000000000
-  AND blockchain = 'ethereum'
-GROUP BY
-  1,2
+select
+    date_trunc('day', evt_block_time) as d,
+    contract_address as token_contract_address,
+    'cbETH' as token_name,
+    sum(value / cast(1e18 as double)) as token_mint_amount
+from
+    evms.erc20_transfers
+where
+    contract_address = 0xbe9895146f7af43049ca1c1ae358b0541ea49704
+    and "from" = 0x0000000000000000000000000000000000000000
+    and blockchain = 'ethereum'
+group by
+    1, 2
--- coinbase/cbeth_supply.sql
+++ coinbase/cbeth_supply.sql
@@ -1,27 +1,32 @@
 /* Dune query number  - 3441005 */
-with days as (select date as day from unnest(sequence(date('2022-02-07'),CURRENT_DATE,interval '1' day)) tbl(date)
+with days as (select date as d from unnest(sequence(date('2022-02-07'), current_date, interval '1' day)) tbl(date)
+),
+
+totals as (
+    select
+        days.d,
+        cast(coalesce(sum(mint.token_mint_amount), 0) as double) as token_mint_amount,
+        cast(coalesce(sum(burn.token_burn_amount), 0) as double) as token_burn_amount,
+        cast(coalesce(sum(mint.token_mint_amount), 0) as double)
+        - cast(coalesce(sum(burn.token_burn_amount), 0) as double) as token_supply_change_amount
+    from days
+    left join
+        query_3440999 as mint
+        on days.d = mint.d
+    left join
+        query_3440997 as burn
+        on days.d = burn.d
+    group by 1
 )
-, totals as (
-    SELECT
-        days.day,
-        cast(coalesce(sum(token_mint_amount),0) as double) as token_mint_amount,
-        cast(coalesce(sum(token_burn_amount),0) as double) as token_burn_amount,
-        cast(coalesce(sum(token_mint_amount),0) as double) - cast(coalesce(sum(token_burn_amount),0) as double) as token_supply_change_amount
-        from days
-        left join 
-          query_3440999 as mint on days.day = mint.day
-        left join 
-          query_3440997 as burn on days.day = burn.day
-        group by 1
-)
-SELECT
-    day,
+
+select
+    d,
     token_mint_amount,
     token_burn_amount,
     token_supply_change_amount,
-    sum(token_mint_amount) over (order by day asc) as token_total_mint,
-    sum(token_burn_amount) over (order by day asc) as token_total_burn,
-    sum(token_supply_change_amount) over (order by day asc) as token_total_supply,
+    sum(token_mint_amount) over (order by d asc) as token_total_mint,
+    sum(token_burn_amount) over (order by d asc) as token_total_burn,
+    sum(token_supply_change_amount) over (order by d asc) as token_total_supply,
     'cbETH' as token_name
 from totals
 order by 1 asc
