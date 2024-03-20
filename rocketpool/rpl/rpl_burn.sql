7ca3046633056161019a54c5e21103842e193961
mtitus6 <84806551+mtitus6@users.noreply.github.com>
2024-03-20 09:50:31 -0400

    lint rpl_mint, rpl_burn

--- rocketpool/rpl/rpl_burn.sql
+++ rocketpool/rpl/rpl_burn.sql
@@ -1,7 +1,7 @@
- /* Dune query number  - 3429927 */
+/* Dune query number  - 3429927 */
  /* Placeholder for any burn events */
- SELECT 
-    cast('2021-10-02' as TIMESTAMP) as day
-    , 0xd33526068d116ce69f19a9ee46f0bd304f21a51f as token_contract_address
-    , 'RPL' as token_name
-    , 0 as token_burn_amount
+select
+    cast('2021-10-02' as timestamp) as d,
+    0xd33526068d116ce69f19a9ee46f0bd304f21a51f as token_contract_address,
+    'RPL' as token_name,
+    0 as token_burn_amount
