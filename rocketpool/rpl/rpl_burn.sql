7289665f2d51965cc2f768a339754ed8f2c2dced
mtitus6 <84806551+mtitus6@users.noreply.github.com>
2024-03-25 21:15:14 -0400

    add pre-commit,  update readme, lint rpl

--- rocketpool/rpl/rpl_burn.sql
+++ rocketpool/rpl/rpl_burn.sql
@@ -1,5 +1,5 @@
-/* Dune query number  - 3429927 */
- /* Placeholder for any burn events */
+/* Dune query number  - 3429927
+Placeholder for any burn events */
 select
     cast('2021-10-02' as timestamp) as d,
     0xd33526068d116ce69f19a9ee46f0bd304f21a51f as token_contract_address,
