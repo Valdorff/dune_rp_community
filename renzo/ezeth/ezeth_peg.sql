e4874406389577cbcbf372d1266ec2477f52c8df
mtitus6 <84806551+mtitus6@users.noreply.github.com>
2024-05-18 08:39:02 -0400

    modify ezeth peg, add apy

--- renzo/ezeth/ezeth_peg.sql
+++ renzo/ezeth/ezeth_peg.sql
@@ -1,12 +1,12 @@
 /* Dune query number  - 3742160 */
 select
-    cast(evt_block_number as bigint) as block,
-    evt_block_time as t,
-    date_trunc('day', evt_block_time) as d,
-    sum(amount) / sum(cast(ezethminted as double)) as token_peg_eth,
+    call_block_number as block,
+    call_block_time as t,
+    date_trunc('day', call_block_time) as d,
+    output_0 / 1e18 as token_peg_eth,
     0xbf5495efe5db9ce00f80364c8b423567e58d2110 as token_contract_address,
     'ezETH' as token_name
 from
-    renzo_ethereum.restakemanager_evt_deposit
-group by
-    1, 2, 3, 5
+    renzo_ethereum.balancerrateprovider_call_getrate
+where
+    call_success = true
