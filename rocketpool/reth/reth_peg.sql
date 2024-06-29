1114e3d74d9297d7184f910bed2934c52a755d35
mtitus6 <84806551+mtitus6@users.noreply.github.com>
2024-06-28 21:31:16 -0400

    Update reth_peg.sql

--- rocketpool/reth/reth_peg.sql
+++ rocketpool/reth/reth_peg.sql
@@ -26,6 +26,22 @@ reth_evt_atlas as (
     where
         contract_address = 0x07fcabcbe4ff0d80c2b1eb42855c0131b6cba2f4
         and topic0 = 0x7bbbb137fdad433d6168b1c75c714c72b8abe8d07460f0c0b433063e7bf1f394
+        and block_time >= cast('2023-04-08 00:00' as timestamp)
+),
+
+reth_evt_houston as (
+    select
+        block_time as t,
+        date_trunc('day', block_time) as d,
+        bytearray_to_uint256(bytearray_substring(data, 1, 32)) as block,
+        bytearray_to_uint256(bytearray_substring(data, 65, 32)) as totaleth,
+        bytearray_to_uint256(bytearray_substring(data, 129, 32)) as rethsupply
+    from
+        ethereum.logs
+    where
+        contract_address = 0x6cc65bf618f55ce2433f9d8d827fc44117d81399
+        and block_time >= cast('2024-06-18 00:00' as timestamp)
+        and topic0 = 0x9b240d5b912ab6df93782930ae851a85b25e5a419c05cbb84d1b9e4b86a3c573
 )
 
 select
@@ -47,4 +63,14 @@ select
     'rETH' as token_name
 from
     reth_evt_atlas
+union
+select
+    t,
+    d,
+    block,
+    totaleth / cast(rethsupply as double) as token_peg_eth,
+    0xae78736cd615f374d3085123a210448e74fc6393 as token_contract_address,
+    'rETH' as token_name
+from
+    reth_evt_houston
 order by 1 asc
