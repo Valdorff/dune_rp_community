d799e2adbbfa541f5c76ca957e4a154e05270929
mtitus6 <84806551+mtitus6@users.noreply.github.com>
2024-09-14 20:56:49 -0400

    add lst_mm_deposit

---
+++ lido/steth/steth_mm_deposit.sql
@@ -0,0 +1,24 @@
+/* Dune query number  - 4057282 */
+with
+txs as (
+    select
+        "from",
+        block_time,
+        value,
+        hash
+    from
+        ethereum.transactions
+    where
+        block_time >= timestamp '2023-02-01'
+)
+
+select
+    txs."from",
+    date(txs.block_time) as d,
+    cast(txs.block_time as timestamp) as t,
+    txs.value / 1e18 as token_deposit_amount,
+    'stETH' as token_name
+from
+    txs
+inner join metamask_ethereum.stakingaggregator_call_deposittolido as mme on txs.hash = mme.call_tx_hash
+where mme.call_success = true
